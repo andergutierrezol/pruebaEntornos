2025a526deb4853869379ed5fd8c147598009dea
in1dm3-a <in1dm3-a@HZ352032.elorrieta.int>
2025-01-13 12:15:37 +0100

    asdasd

---
+++ PruebadeEntornos/src/PrEntornos/PrEntornos.java
@@ -0,0 +1,10 @@
+package PrEntornos;
+
+public class PrEntornos {
+
+	public static void main(String[] args) {
+		 System.out.println("holu");
+
+	}
+
+}
---
+++ PruebadeEntornos/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module PruebadeEntornos {
+}
